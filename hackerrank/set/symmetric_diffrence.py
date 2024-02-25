c307338f555b7985f9fdb3106b1a8764c4b9d755
Mukund <mukundkushwaha395@gmail.com>
2024-02-26 00:09:22 +0530

    practcing set

---
+++ hackerrank/set/symmetric_diffrence.py
@@ -0,0 +1,36 @@
+# n  =list(map(int,input("enter the list of number - ").split(" ")))
+# print(n)
+
+# myset =set(["a","b"])
+# myset.add("c")
+# # myset.add((3,5))
+# myset.add(1)
+# myset.update("d","e",(2,3))
+# myset.discard(3)
+# myset.remove("e")
+# print(myset)
+
+
+#
+# s = {2,4,5,9}
+# s1 = {2,4,11,12}
+# l =s.symmetric_difference(s1)
+# print(type(l))
+
+#
+# k = ['pop', 'remove 9', 'discard 9', 'discard 8', 'remove 7', 'pop', 'discard 6', 'remove 5', 'pop', 'discard 5']
+# s  ={1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+# for i in range(len(k)):
+#     if k[i] =="pop":
+#         s.pop()
+#     p=(k[i].split())
+#     if p[0]=="remove":
+#         s.remove(int(p[1]))
+#     if p[0]=="discard":
+#         s.discard(int(p[1]))
+# print(sum(s))
+
+
+# n3 = set(map(int,input().split()))
+# print(n3)
